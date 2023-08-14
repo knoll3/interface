e5d8107792f06e910199d7f37130540cabe6ea75
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-08-14 13:50:57 +0100

    Added filter button functionality

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -17,9 +17,25 @@ import { useAccount } from 'wagmi';
 import { Image, CreditCard, Chat, Scorecard } from 'grommet-icons';
   
 
+const cardColors = {
+    "Large Language Model Finetuning": "#A4C0FF",
+    "NLP": "#E69FBD",
+    "Time series prediction": "#D9D9D9",
+    "Classification": "#BDD4DA",
+}
+
 export default function MarketplacePage() {
     const { isDisconnected } = useAccount();
     const size = useContext(ResponsiveContext);
+    const [filter, setFilter] = useState<string[]>([]);
+
+    const filterAction = (item: string) => {
+        if (filter.find((f) => f === item)) {
+          setFilter(filter.filter((f) => f !== item));
+        } else {
+          setFilter([...filter, item]);
+        }
+    }
 
     return (
         <Layout>
@@ -31,25 +47,28 @@ export default function MarketplacePage() {
                             border={{ color: 'black', size: 'small' }}
                             round="small"
                             pad="xsmall"
+                            background={ filter.includes('NLP') ? cardColors["NLP"] : ''}
                             direction="row"
                             gap="small"
                             align="center"
+                            onClick={() => filterAction('NLP')}
                         >
-                            <Scorecard color="black" size="20px" /><Text weight="bold">Sentiment Analysis</Text>
+                            <Scorecard color="black" size="20px" /><Text weight="bold">NLP</Text>
                         </Box>
                         <Box
                             border={{ color: 'black', size: 'small' }}
                             round="small"
                             pad="xsmall"
-                            background="#E69FBD"
+                            background={ filter.includes('Large Language Model Finetuning') ? cardColors["Large Language Model Finetuning"] : ''}
                             direction="row"
                             gap="small"
                             align="center"
+                            onClick={() => filterAction('Large Language Model Finetuning')}
                         >
-                            <Chat color="black" size="20px" /><Text weight="bold">LLM Chatbot</Text>
+                            <Chat color="black" size="20px" /><Text weight="bold">LLM Finetuning</Text>
                         </Box>
                     </Box>
-                    <Box>
+                    <Box gap="small">
                         <Heading level="3">Finance</Heading>
                         <Box
                             border={{ color: 'black', size: 'small' }}
@@ -58,21 +77,36 @@ export default function MarketplacePage() {
                             direction="row"
                             gap="small"
                             align="center"
+                            onClick={() => filterAction('Credit Card Fraud Detection')}
                         >
                             <CreditCard color="black" size="20px" /><Text weight="bold">Credit Card Fraud Detection</Text>
                         </Box>
+                        <Box
+                            border={{ color: 'black', size: 'small' }}
+                            round="small"
+                            pad="xsmall"
+                            background={ filter.includes('Time series prediction') ? cardColors["Time series prediction"] : ''}
+                            direction="row"
+                            gap="small"
+                            align="center"
+                            onClick={() => filterAction('Time series prediction')}
+                        >
+                            <CreditCard color="black" size="20px" /><Text weight="bold">Time series prediction</Text>
+                        </Box>
                     </Box>
-                    <Box>
+                    <Box gap="small">
                         <Heading level="3">Computer Vision</Heading>
                         <Box
                             border={{ color: 'black', size: 'small' }}
                             round="small"
                             pad="xsmall"
+                            background={ filter.includes('Classification') ? cardColors["Classification"] : ''}
                             direction="row"
                             gap="small"
                             align="center"
+                            onClick={() => filterAction('Classification')}
                         >
-                            <Image color="black" size="20px" /><Text weight="bold">Image Classification</Text>
+                            <Image color="black" size="20px" /><Text weight="bold">Classification</Text>
                         </Box>
                     </Box>
                 </Box>
@@ -97,7 +131,7 @@ export default function MarketplacePage() {
                         </Box>
                     </Box>
                     <Box pad={{ horizontal: 'large'}} align="center" height={{ min: 'large'}}>
-                        <MarketplaceItems />
+                        <MarketplaceItems filterItems={filter} />
                     </Box>
                 </Box>
             </Box>
--- src/pages/train.tsx
+++ src/pages/train.tsx
@@ -14,11 +14,27 @@ import { useState, useContext } from 'react';
 import { Chat, CreditCard, Scorecard, Search, Image } from 'grommet-icons';
 import { useAccount } from 'wagmi';
 
+const cardColors = {
+  "Large Language Model Finetuning": "#A4C0FF",
+  "NLP": "#E69FBD",
+  "Time series prediction": "#D9D9D9",
+  "Classification": "#BDD4DA",
+}
+
 export default function TrainPage() {
   const [showCreateTask, setShowCreateTask] = useState(false);
   const [numberOfTasks, setNumberOfTasks] = useState(0);
+  const [filter, setFilter] = useState<string[]>([]);
   const { isDisconnected } = useAccount();
   const size = useContext(ResponsiveContext);
+
+  const filterAction = (item: string) => {
+    if (filter.find((f) => f === item)) {
+      setFilter(filter.filter((f) => f !== item));
+    } else {
+      setFilter([...filter, item]);
+    }
+  }
 
   return (
     <Layout>
@@ -40,25 +56,28 @@ export default function TrainPage() {
                   border={{ color: 'black', size: 'small' }}
                   round="small"
                   pad="xsmall"
+                  background={ filter.includes('NLP') ? cardColors["NLP"] : ''}
                   direction="row"
                   gap="small"
                   align="center"
+                  onClick={() => filterAction('NLP')}
               >
-                  <Scorecard color="black" size="20px" /><Text weight="bold">Sentiment Analysis</Text>
+                  <Scorecard color="black" size="20px" /><Text weight="bold">NLP</Text>
               </Box>
               <Box
                   border={{ color: 'black', size: 'small' }}
                   round="small"
                   pad="xsmall"
-                  background="#E69FBD"
+                  background={ filter.includes('Large Language Model Finetuning') ? cardColors["Large Language Model Finetuning"] : ''}
                   direction="row"
                   gap="small"
                   align="center"
+                  onClick={() => filterAction('Large Language Model Finetuning')}
               >
-                  <Chat color="black" size="20px" /><Text weight="bold">LLM Chatbot</Text>
+                  <Chat color="black" size="20px" /><Text weight="bold">LLM Finetuning</Text>
               </Box>
           </Box>
-          <Box>
+          <Box gap="small">
               <Heading level="3">Finance</Heading>
               <Box
                   border={{ color: 'black', size: 'small' }}
@@ -67,21 +86,36 @@ export default function TrainPage() {
                   direction="row"
                   gap="small"
                   align="center"
+                  onClick={() => filterAction('Credit Card Fraud Detection')}
               >
                   <CreditCard color="black" size="20px" /><Text weight="bold">Credit Card Fraud Detection</Text>
               </Box>
+              <Box
+                  border={{ color: 'black', size: 'small' }}
+                  round="small"
+                  pad="xsmall"
+                  background={ filter.includes('Time series prediction') ? cardColors["Time series prediction"] : ''}
+                  direction="row"
+                  gap="small"
+                  align="center"
+                  onClick={() => filterAction('Time series prediction')}
+              >
+                  <CreditCard color="black" size="20px" /><Text weight="bold">Time series prediction</Text>
+              </Box>
           </Box>
-          <Box>
+          <Box gap="small">
               <Heading level="3">Computer Vision</Heading>
               <Box
                   border={{ color: 'black', size: 'small' }}
                   round="small"
                   pad="xsmall"
+                  background={ filter.includes('Classification') ? cardColors["Classification"] : ''}
                   direction="row"
                   gap="small"
                   align="center"
+                  onClick={() => filterAction('Classification')}
               >
-                  <Image color="black" size="20px" /><Text weight="bold">Image Classification</Text>
+                  <Image color="black" size="20px" /><Text weight="bold">Classification</Text>
               </Box>
           </Box>
         </Box>
@@ -106,7 +140,7 @@ export default function TrainPage() {
               </Box>
           </Box>
           <Box pad={{ horizontal: 'large'}} align="center">
-            <Tasks setNumberOfTasks={setNumberOfTasks} />
+            <Tasks setNumberOfTasks={setNumberOfTasks} filterItems={filter} />
           </Box>
         </Box>
       </Box>
