e8520f945d6600ac0eb11cc35aa12d1582ff8f1c
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-09-14 11:48:06 +0100

    Added external wallet support

--- src/hooks/userDataHook.ts
+++ src/hooks/userDataHook.ts
@@ -1,30 +1,38 @@
 import { useState, useEffect } from 'react';
 import { web3AuthInstance } from '@/src/hooks/web3AuthInstance';
 import { getPublicCompressed } from "@toruslabs/eccrypto";
+import { useAccount } from 'wagmi';
 
 export const userDataHook = () => {
-    const [userEmail, setUserEmail] = useState<string>("");
     const [userToken, setUserToken] = useState<string>("");
     const [publicKey, setPublicKey] = useState<string>("");
+    const { address } = useAccount();
 
 
     const loadUserData = async () => {
         const user = await web3AuthInstance.getUserInfo();
-        const privateKey = await web3AuthInstance.provider?.request({
-            method: "eth_private_key", 
-        }) as string;
-    
-        const publicKey = getPublicCompressed(Buffer.from(privateKey.padStart(64, "0"), "hex")).toString("hex");
 
-        setUserEmail(user.email!);
-        setUserToken(user.idToken!);
-        setPublicKey(publicKey);
+        if (Object.keys(user).length === 0) {
+            const { idToken } = await web3AuthInstance.authenticateUser();
+            
+            setUserToken(idToken);
+            setPublicKey(address!.toLocaleLowerCase());
+        } else {
+            const privateKey = await web3AuthInstance.provider?.request({
+                method: "eth_private_key", 
+            }) as string;
+        
+            const publicKey = getPublicCompressed(Buffer.from(privateKey.padStart(64, "0"), "hex")).toString("hex");
+
+            setUserToken(user.idToken!);
+            setPublicKey(publicKey);
+        }
     };
     
     useEffect(() => {
-        if (web3AuthInstance.connected)
+        if (web3AuthInstance.connected && address)
             loadUserData();
-    }, [web3AuthInstance.connected]);
+    }, [web3AuthInstance.connected, address]);
 
-    return { userEmail, userToken, publicKey };
+    return { userToken, publicKey };
 };
