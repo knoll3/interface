50a6b0b5c1518a0f93898e66e58b9977e911d7e3
Caique Ribas <mechamocaique@gmail.com>
2023-09-18 16:11:57 -0300

    chore: fix middleware, add public key verification for social logins

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,75 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
-import type { NextApiRequest, NextApiResponse } from 'next';
 import * as jose from "jose"
-import { web3AuthInstance } from '@/src/hooks';
+import { isStringObject } from 'util/types'
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(req: NextRequest) {
+async function verifyByPublicKey(req: NextRequest) {
+  // passed from the frontend in the Authorization header
+  const idToken = req.headers.get("Authorization")?.split(' ')[1]
+
+  // passed from the frontend in the request body
+  const app_pub_key = (await req.json())["user_key"]
+
+  if (isStringObject(idToken)) {
+    try {
+      const jwksSocial = jose.createRemoteJWKSet(
+        new URL('https://api.openlogin.com/jwks')
+      );
+
+      // Verify the JWT using Web3Auth's JWKS
+      const jwtDecodedSocial = await jose.jwtVerify(idToken!, jwksSocial, {
+        algorithms: ['ES256'],
+      });
+
+      if ((jwtDecodedSocial.payload as any).wallets[0].public_key === app_pub_key) {
+        return NextResponse.next()
+      }
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        { message: `${app_pub_key}: 'Verification failed'` },
+        { status: 503, headers: { 'content-type': 'application/json' } })
+    }
+  }
+}
+
+async function verifyByWalletAddress(req: NextRequest) {
+  // passed from the frontend in the Authorization header
   const idToken = req.headers.get("Authorization")?.split(' ')[1];
-  const publicAddress = (await req.json())["public_address"]
 
-  if (idToken != undefined) {
+  // passed from the frontend in the request body. Cloud be either a wallet_address or public_address
+  const wallet_address = (await req.json())["user_key"];
+
+  if (isStringObject(idToken)) {
     const jwks = jose.createRemoteJWKSet(new URL("https://authjs.web3auth.io/jwks"));
     try {
       const jwtDecoded = await jose.jwtVerify(idToken, jwks, { algorithms: ["ES256"] });
 
-      if ((jwtDecoded.payload as any).wallets[0].address != publicAddress) {
-        return NextResponse.json({ message: `${publicAddress}: 'Verification failed'` },
-          { status: 401, headers: { 'content-type': 'application/json' } }
-        )
+      if ((jwtDecoded.payload as any).wallets[0].address == wallet_address) {
+        return NextResponse.next()
       }
     } catch (error) {
-      return NextResponse.json({ message: `${publicAddress}: 'Verification failed'` },
-        { status: 401, headers: { 'content-type': 'application/json' } }
-      )
+      console.error(error);
+      return NextResponse.json(
+        { message: `${wallet_address}: 'Verification failed'` },
+        { status: 503, headers: { 'content-type': 'application/json' } })
     }
   }
-  return NextResponse.next()
+}
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(req: NextRequest) {
+
+  await verifyByPublicKey(req)
+  await verifyByWalletAddress(req)
+
+  // in case of bypass through verifications, it might mean that no tokenId was received
+  const user_key = (await req.json())["user_key"]
+  return NextResponse.json(
+    { message: `${user_key}: 'Verification failed'` },
+    { status: 401, headers: { 'content-type': 'application/json' } })
 }
 
 export const config = {
-  matcher: '/api/quest/:path*',
+  matcher: ['/api/quest/:path*'],
 }
