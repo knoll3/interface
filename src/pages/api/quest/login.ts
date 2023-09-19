b02932e9ed90a86b13229259b4ca45cee729c0a7
rodrigopavezi <rodrigopavezi@gmail.com>
2023-09-19 09:10:36 -0300

    fix auth key params

--- src/pages/api/quest/login.ts
+++ src/pages/api/quest/login.ts
@@ -1,49 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import * as jose from "jose"
-import { web3AuthInstance } from '@/src/hooks';
-import { PrismaClient, User } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 type Response = {
   data: {
-    message: string
-  }
+    message: string;
+  };
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
-
-  const prismaDB = new PrismaClient()
-  await prismaDB.$connect()
+  const prismaDB = new PrismaClient();
+  await prismaDB.$connect();
 
   try {
     let user = await prismaDB.user.findUnique({
       where: {
-        wallet: req.body.user_key
+        wallet: req.body.wallet,
       },
       select: {
         wallet: true,
-        userQuestTask: true
-      }
-    })
-    console.log(user)
+        userQuestTask: true,
+      },
+    });
+    console.log(user);
 
-    if (!user){
+    if (!user) {
       user = await prismaDB.user.create({
         data: {
-          wallet: req.body.user_key
+          wallet: req.body.wallet,
         },
         select: {
           wallet: true,
-          userQuestTask: true
-        }
-      })
-      console.log(user)
+          userQuestTask: true,
+        },
+      });
+      console.log(user);
     }
-    res.status(200).json({data: {message: "OK"}})
-  } catch (error){
-    console.log(error)
-    res.status(500).json({data: {message: "Internal Server Error"}})
+    res.status(200).json({ data: { message: 'OK' } });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ data: { message: 'Internal Server Error' } });
   }
 }
