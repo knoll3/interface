f9d030726e4264800d246927c2d1fd5db69dccad
MIDAV0 <magilnyy.vadimo@gmail.com>
2023-09-29 12:57:06 +0100

    Functions

--- src/pages/gptResearcher.tsx
+++ src/pages/gptResearcher.tsx
@@ -12,11 +12,100 @@ import {
     TextArea,
     TextInput 
 } from 'grommet';
+import { useState } from "react";
 
 export default function GptResearcherPage() {
 
-    const handleSubmit = (event: any) => {
-        console.log("Form submitted");
+    const [report, setReport] = useState<string>("");
+    const [agentOutput, setAgentOutput] = useState<string>("");
+
+    const GPTResearcher = (() => {
+        const startResearch = () => {
+          addAgentResponse({ output: "🤔 Too many requests right now, you are in the queue, please be patient." });
+      
+          listenToSockEvents();
+        };
+      
+        const listenToSockEvents = () => {
+          const { protocol, host, pathname } = window.location;
+          const ws_uri = `${protocol === 'https:' ? 'wss:' : 'ws:'}//${host}${pathname}ws`;
+          const converter = new showdown.Converter();
+          const socket = new WebSocket(ws_uri);
+      
+          socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if (data.type === 'logs') {
+              addAgentResponse(data);
+            } else if (data.type === 'report') {
+              writeReport(data, converter);
+            } else if (data.type === 'path') {
+              updateDownloadLink(data);
+            }
+          };
+      
+          socket.onopen = (event) => {
+            const task = document.querySelector('input[name="task"]').value;
+            const report_type = document.querySelector('select[name="report_type"]').value;
+            const agent = document.querySelector('input[name="agent"]:checked').value;
+      
+            const requestData = {
+              task: task,
+              report_type: report_type,
+              agent: agent,
+            };
+      
+            socket.send(`start ${JSON.stringify(requestData)}`);
+          };
+        };
+      
+        const addAgentResponse = (data) => {
+          const output = document.getElementById("output");
+          output.innerHTML += '<div class="agent_response">' + data.output + '</div>';
+          output.scrollTop = output.scrollHeight;
+          output.style.display = "block";
+          updateScroll();
+        };
+      
+        const writeReport = (data, converter) => {
+          const reportContainer = document.getElementById("reportContainer");
+          const markdownOutput = converter.makeHtml(data.output);
+          reportContainer.innerHTML += markdownOutput;
+          updateScroll();
+        };
+      
+        const updateDownloadLink = (data) => {
+          const path = data.output;
+          const downloadLink = document.getElementById("downloadLink");
+          downloadLink.href = path;
+        };
+      
+        const updateScroll = () => {
+          window.scrollTo(0, document.body.scrollHeight);
+        };
+      
+        const copyToClipboard = () => {
+          const textarea = document.createElement('textarea');
+          textarea.id = 'temp_element';
+          textarea.style.height = 0;
+          document.body.appendChild(textarea);
+          textarea.value = document.getElementById('reportContainer').innerText;
+          const selector = document.querySelector('#temp_element');
+          selector.select();
+          document.execCommand('copy');
+          document.body.removeChild(textarea);
+        };
+      
+        return {
+          startResearch,
+          copyToClipboard,
+        };
+      })();
+    
+
+    const handleSubmit = () => {
+        setReport("");
+        setAgentOutput("");
+        GPTResearcher.startResearch();
     };
 
     return (
@@ -67,13 +156,13 @@ export default function GptResearcherPage() {
                             An agent tailored specifically to your task will be generated to provide the most precise and relevant research results.
                         </Text>
                         <Box height="300px">
-                            <TextArea resize={false} fill/>
+                            <TextArea resize={false} fill value={agentOutput}/>
                         </Box>
                     </Box>
                     <Box width="100%" margin={{ top: 'medium'}} gap="small">
                         <Heading level="2" margin="xsmall">Research Report</Heading>
                         <Box width="100%" border height={{min: '30px'}} round="small">
-                            <Text></Text>
+                            <Text>{report}</Text>
                         </Box>
                         <Box direction="row-responsive" gap="small">
                             <Button
