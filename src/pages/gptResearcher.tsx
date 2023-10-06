b8a7b9725e915890703c74ad95792e0d196c420e
MIDAV0 <magilnyy.vadimo@gmail.com>
2023-10-06 12:16:28 +0100

    Uncommented

--- src/pages/gptResearcher.tsx
+++ src/pages/gptResearcher.tsx
@@ -122,8 +122,7 @@ export default function GptResearcherPage() {
     });
 
     const handlePurchase = async () => {
-        console.log("Purchase");
-        //writePurchaseCredits?.({ args: [amount]});
+        writePurchaseCredits?.({ args: [amount]});
     };
 
     return (
