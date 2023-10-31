356495474f13e37c599cc03de3258e91e4098dac
MIDAV0 <magilnyy.vadimo@gmail.com>
2023-10-31 18:15:02 +0000

    Adjusting components

--- src/components/Researcher/Research.tsx
+++ src/components/Researcher/Research.tsx
@@ -1,56 +1,55 @@
-import { Box, Heading, Select, Text, TextInput } from 'grommet';
-import { Add, Copy } from 'grommet-icons';
+import { Box, Button, Heading, Layer, RangeInput, Select, Text, TextInput } from 'grommet';
 import { PrimaryButton } from '../PrimaryButton';
+import { useState } from 'react';
 
-type ReportProps = {
-    reportType: string;
-    reportTitle: string;
-    reportLink: string;
-}
 
 export const Research = () => {
+    const [reportType, setReportType] = useState({
+        label: 'Outline Report',
+        value: 'outline_report',
+      });
 
   return (
     <>
-    <Box
-          pad={{ vertical: 'large', horizontal: 'large' }}
-          gap="medium"
-          fill="horizontal"
-    >
-        <Box>
+        <Box
+            pad={{ vertical: 'large', horizontal: 'large' }}
+            gap="medium"
+            fill="horizontal"
+        >
             <Box>
-                <Heading level="2" margin="none" weight="bold">Step 1: Research & Download Reports</Heading>
-                <Text><span>&#42;</span> You need to pay $FLC {} to start your research</Text>
-            </Box>
-        </Box>
-        <Box gap="small">
-            <Box>
-              <Text>What would you like me to research next?</Text>
-              <Box background="white">
-                <TextInput />
-              </Box>
-            </Box>
-            <Box>
-                <Text>What type of report would you like me to generate?</Text>
-                <Box background="white">
-                    <Select
-                        options={[
-                        { label: 'Outline Report', value: 'outline_report' },
-                        { label: 'Research Report', value: 'research_report' },
-                        { label: 'Resource Report', value: 'resource_report' },
-                        ]}
-                        // value={reportType.value}
-                        // valueLabel={<Box pad="small">{reportType.label}</Box>}
-                        // onChange={({ option }) => setReportType(option)}
-                        // disabled={isResearching}
-                    />
+                <Box>
+                    <Heading level="2" margin="none" weight="bold">Step 1: Research & Download Reports</Heading>
+                    <Text><span>&#42;</span> You need to pay $FLC {} to start your research</Text>
                 </Box>
             </Box>
+            <Box gap="small">
+                <Box>
+                <Text>What would you like me to research next?</Text>
+                <Box background="white">
+                    <TextInput />
+                </Box>
+                </Box>
+                <Box>
+                    <Text>What type of report would you like me to generate?</Text>
+                    <Box background="white">
+                        <Select
+                            options={[
+                            { label: 'Outline Report', value: 'outline_report' },
+                            { label: 'Research Report', value: 'research_report' },
+                            { label: 'Resource Report', value: 'resource_report' },
+                            ]}
+                            // value={reportType.value}
+                            // valueLabel={<Box pad="small">{reportType.label}</Box>}
+                            // onChange={({ option }) => setReportType(option)}
+                            // disabled={isResearching}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+            <Box justify="end" align="end">
+                <PrimaryButton label="Pay and research" />
+            </Box>
         </Box>
-        <Box justify="end" align="end">
-            <PrimaryButton label="Pay and research" />
-        </Box>
-    </Box>
-  </>
+    </>
   );
 };
