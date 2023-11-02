be3dd68fe63ce03ef133adabe296a0ffac4e88d5
Elizabeth Lui <95862857+elihylui@users.noreply.github.com>
2023-11-02 14:37:00 +0000

    update contract address & abi (#96)

--- src/contracts/flockCredits.ts
+++ src/contracts/flockCredits.ts
@@ -1,324 +1,324 @@
 export const FLOCK_CREDITS_ABI = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_flockTokenAddress',
-        type: 'address',
+        "internalType": "address",
+        "name": "_flockTokenAddress",
+        "type": "address"
       },
       {
-        internalType: 'address',
-        name: '_flockNFTAddress',
-        type: 'address',
+        "internalType": "address",
+        "name": "_flockNFTAddress",
+        "type": "address"
       },
       {
-        internalType: 'uint256',
-        name: '_price',
-        type: 'uint256',
-      },
+        "internalType": "uint256",
+        "name": "_price",
+        "type": "uint256"
+      }
     ],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: 'address',
-        name: 'previousOwner',
-        type: 'address',
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
       },
       {
-        indexed: true,
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address',
-      },
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
     ],
-    name: 'OwnershipTransferred',
-    type: 'event',
+    "name": "OwnershipTransferred",
+    "type": "event"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'uint256',
-        name: '_amount',
-        type: 'uint256',
-      },
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
     ],
-    name: 'addCredits',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    "name": "addCredits",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      }
     ],
-    name: 'checkBalance',
-    outputs: [
+    "name": "checkBalance",
+    "outputs": [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      }
     ],
-    name: 'checkIfWhiteListed',
-    outputs: [
+    "name": "checkIfWhiteListed",
+    "outputs": [
       {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      }
     ],
-    name: 'checkNFT',
-    outputs: [
+    "name": "checkNFT",
+    "outputs": [
       {
-        components: [
+        "components": [
           {
-            internalType: 'string',
-            name: 'name',
-            type: 'string',
+            "internalType": "string",
+            "name": "name",
+            "type": "string"
           },
           {
-            internalType: 'uint256[]',
-            name: 'tokenIds',
-            type: 'uint256[]',
+            "internalType": "uint256[]",
+            "name": "tokenIds",
+            "type": "uint256[]"
           },
           {
-            internalType: 'uint256',
-            name: 'voteCount',
-            type: 'uint256',
+            "internalType": "uint256",
+            "name": "voteCount",
+            "type": "uint256"
           },
           {
-            internalType: 'address[]',
-            name: 'voters',
-            type: 'address[]',
-          },
+            "internalType": "address[]",
+            "name": "voters",
+            "type": "address[]"
+          }
         ],
-        internalType: 'struct FLockNFT.Agent[]',
-        name: '',
-        type: 'tuple[]',
-      },
+        "internalType": "struct FLockNFT.Agent[]",
+        "name": "",
+        "type": "tuple[]"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_userAddress',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "_userAddress",
+        "type": "address"
+      }
     ],
-    name: 'consumeCredits',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    "name": "consumeCredits",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'flockNFT',
-    outputs: [
+    "inputs": [],
+    "name": "flockNFT",
+    "outputs": [
       {
-        internalType: 'contract FLockNFT',
-        name: '',
-        type: 'address',
-      },
+        "internalType": "contract FLockNFT",
+        "name": "",
+        "type": "address"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'flockToken',
-    outputs: [
+    "inputs": [],
+    "name": "flockToken",
+    "outputs": [
       {
-        internalType: 'contract FlockToken',
-        name: '',
-        type: 'address',
-      },
+        "internalType": "contract FlockToken",
+        "name": "",
+        "type": "address"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'owner',
-    outputs: [
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'price',
-    outputs: [
+    "inputs": [],
+    "name": "price",
+    "outputs": [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      }
     ],
-    name: 'removeUsersFromWhiteList',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    "name": "removeUsersFromWhiteList",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'renounceOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'uint256',
-        name: '_newPrice',
-        type: 'uint256',
-      },
+        "internalType": "uint256",
+        "name": "_newPrice",
+        "type": "uint256"
+      }
     ],
-    name: 'setPrice',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    "name": "setPrice",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
     ],
-    name: 'transferOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_addressToCheck',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "_addressToCheck",
+        "type": "address"
+      }
     ],
-    name: 'userExists',
-    outputs: [
+    "name": "userExists",
+    "outputs": [
       {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
-    name: 'users',
-    outputs: [
+    "name": "users",
+    "outputs": [
       {
-        internalType: 'bool',
-        name: 'exists',
-        type: 'bool',
+        "internalType": "bool",
+        "name": "exists",
+        "type": "bool"
       },
       {
-        internalType: 'address',
-        name: 'wallet',
-        type: 'address',
+        "internalType": "address",
+        "name": "wallet",
+        "type": "address"
       },
       {
-        internalType: 'uint256',
-        name: 'balance',
-        type: 'uint256',
+        "internalType": "uint256",
+        "name": "balance",
+        "type": "uint256"
       },
       {
-        internalType: 'bool',
-        name: 'isWhiteListed',
-        type: 'bool',
-      },
+        "internalType": "bool",
+        "name": "isWhiteListed",
+        "type": "bool"
+      }
     ],
-    stateMutability: 'view',
-    type: 'function',
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      }
     ],
-    name: 'whiteListUsers',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    "name": "whiteListUsers",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'uint256',
-        name: '_amount',
-        type: 'uint256',
-      },
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
     ],
-    name: 'withdrawFunds',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
+    "name": "withdrawFunds",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
 ];
