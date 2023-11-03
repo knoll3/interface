d9eaa25158b2a9ca3a7083adc8b36a67681619ba
MIDAV0 <magilnyy.vadimo@gmail.com>
2023-11-03 12:10:17 +0000

    Reposition the agent output

--- src/pages/gptResearcher.tsx
+++ src/pages/gptResearcher.tsx
@@ -148,7 +148,8 @@ export default function GptResearcherPage() {
     };
 
     const listenToSockEvents = () => {
-      const ws_uri = `${process.env.NEXT_PUBLIC_RESEARCHER_WEB_SOCKET_URL}?token=${userToken}&authKey=${publicKey}`;
+      // const ws_uri = `${process.env.NEXT_PUBLIC_RESEARCHER_WEB_SOCKET_URL}?token=${userToken}&authKey=${publicKey}`;
+      const ws_uri = `ws://localhost/ws?token=${userToken}&authKey=${publicKey}`
       const socket = new WebSocket(ws_uri);
       socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
