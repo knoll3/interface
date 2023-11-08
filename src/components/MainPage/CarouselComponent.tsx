31ca68751a73bd07345f8da6d73a1bc3f63909d3
MIDAV0 <magilnyy.vadimo@gmail.com>
2023-11-08 12:25:54 +0000

    Updated carousel

--- src/components/MainPage/CarouselComponent.tsx
+++ src/components/MainPage/CarouselComponent.tsx
@@ -1,16 +1,20 @@
-import { Box, Heading, Text, Image, Carousel } from 'grommet';
+import { Box, Heading, Text, Image, Carousel, Button } from 'grommet';
+import { useState } from 'react';
 
 export const CarouselComponent = () => {
 
+  const [activeSlide, setActiveSlide] = useState(0);
+  
   return (
     <>
     <Box pad={{ vertical: 'large', horizontal: 'large' }} fill>
-      <Box round="small" fill="vertical">
+      <Box round="small">
         <Carousel
-          fill
-          controls="selectors"
+          controls={false}
           wrap
           play={4000}
+          activeChild={activeSlide}
+          onChild={setActiveSlide}
         >
           <Box pad="medium" >
             <Image fill src="CarouselFrame1.png" alt="carousel image" />
@@ -23,6 +27,18 @@ export const CarouselComponent = () => {
           </Box>
         </Carousel>
       </Box>
+      <Box 
+        alignSelf="center"
+        width="10%"
+        direction="row"
+        align='center'
+        justify="center"
+        gap="medium"
+      >
+        <Button fill color={activeSlide === 0 ? "brand" : "white"} onClick={() => setActiveSlide(0)} />
+        <Button fill color={activeSlide === 1 ? "brand" : "white"} onClick={() => setActiveSlide(1)} />
+        <Button fill color={activeSlide === 2 ? "brand" : "white"} onClick={() => setActiveSlide(2)} />
+      </Box>        
     </Box>
   </>
   );
