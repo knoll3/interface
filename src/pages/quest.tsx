840769dc414fbc6374c80e219c52d57ab9428de7
Kyle Noll <noll.kyle@gmail.com>
2023-11-18 17:01:58 -0800

    Fix hidden content on quest page

--- src/pages/quest.tsx
+++ src/pages/quest.tsx
@@ -37,7 +37,6 @@ export default function QuestPage() {
   const size = useContext(ResponsiveContext);
   const { toasts, addToast } = useToaster();
   const isMobile = size === 'small';
-  const isDesktop = size === 'large';
 
   const showToaster = ({ toast }: IOnSubmitProps) => addToast(toast);
 
@@ -51,7 +50,6 @@ export default function QuestPage() {
           pad={{ vertical: 'large', horizontal: 'xlarge' }}
           background="white"
           gap="30px"
-          height={isDesktop ? 'calc(100vh - 450px)' : undefined}
         >
           <ToasterList toasts={toasts} />
           <Box gap="large" width={isMobile ? '300px' : undefined}>
